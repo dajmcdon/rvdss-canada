51782f1e0f330952e2bed0916ac01da8a6d8f74d
cchuong <cchuong2000@gmail.com>
2024-11-28 09:22:55 -0800

    update current data and update script

--- src/rvdss_update.py
+++ src/rvdss_update.py
@@ -6,82 +6,48 @@ from epiweeks import Week
 from datetime import datetime
 import math
 import os
+from unidecode import unidecode
+import string
+
+
+from src.constants import (
+        VIRUSES, GEOS, REGIONS, NATION,
+        DASHBOARD_UPDATE_DATE_FILE, DASHBOARD_DATA_FILE, DASHBOARD_BASE_URL
+    )
 
 def abbreviate_virus(full_name):
     lowercase=full_name.lower()
-    
-    if any(name in lowercase for name in ["parainfluenza","para","piv"]):
-        if "hpiv" not in lowercase:
-            abbrev = re.sub("parainfluenza|para|piv","hpiv",lowercase)
-        else:
-            abbrev = lowercase
-    elif any(name in lowercase for name in ["adenovirus","adeno"]):
-        abbrev =  re.sub("adenovirus|adeno","adv",lowercase)
-    elif "human metapneumovirus" in lowercase:
-        abbrev =  re.sub("human metapneumovirus","hmpv",lowercase)
-    elif any(name in lowercase for name in ["enterovirus/rhinovirus","rhinovirus","rhv","entero/rhino","rhino","ev/rv","evrv"]):
-        abbrev = re.sub("enterovirus/rhinovirus|rhinovirus|rhv|entero/rhino|rhino|ev/rv|evrv","ev_rv",lowercase)
-    elif any(name in lowercase for name in ["coronavirus","coron","coro"]):
-        abbrev = re.sub("coronavirus|coron|coro","hcov",lowercase)
-    elif "respiratory syncytial virus" in lowercase:
-        abbrev = re.sub("respiratory syncytial virus","rsv",lowercase)
-    elif "influenza" in lowercase:
-        abbrev = re.sub("influenza","flu",lowercase)       
-    elif "sarscov2" in lowercase:
-        abbrev = re.sub("sarscov2","sars-cov-2",lowercase) 
-    else:
-        abbrev=lowercase
-    return(abbrev)
+    keys = (re.escape(k) for k in VIRUSES.keys())
+    pattern = re.compile(r'\b(' + '|'.join(keys) + r')\b')
+    result = pattern.sub(lambda x: VIRUSES[x.group()], lowercase)
+    return(result)
 
 def abbreviate_geo(full_name):
     lowercase=full_name.lower()
+    lowercase = re.sub("province of ","",lowercase)
+    lowercase=re.sub("\.|\*","",lowercase)
+    lowercase=re.sub("/territoires","",lowercase) 
+    lowercase=re.sub("^cana$","can",lowercase)
+    lowercase =lowercase.translate(str.maketrans(string.punctuation, ' '*len(string.punctuation),'.'+"'"))
+    lowercase=re.sub("kidshospital","kids hospital",lowercase)
+    lowercase=re.sub(' +', ' ', lowercase)
     
-    if "newfoundland" in lowercase:
-        abbrev =  "nl"
-    elif "prince edward island" in lowercase:
-        abbrev =  "pe"
-    elif "nova scotia" in lowercase:
-        abbrev =  "ns"
-    elif "new brunswick" in lowercase:
-        abbrev =  "nb"
-    elif re.match('|'.join(("^québec$", "province of québec","quebec")),lowercase):
-        abbrev = "qc"  
-    elif re.match('|'.join(("^ontario$", "province of ontario")),lowercase):
-        abbrev =  "on"
-    elif "manitoba" in lowercase:
-        abbrev =  "mb"
-    elif "saskatchewan" in lowercase:
-        abbrev =  "sk"
-    elif "alberta" in lowercase:
-        abbrev =  "ab"
-    elif "british columbia" in lowercase:
-        abbrev =  "bc"
-    elif "yukon" in lowercase:
-        abbrev =  "yk"
-    elif "northwest territories" in lowercase:
-        abbrev =  "nt"
-    elif "nunavut" in lowercase:
-        abbrev =  "nu"
-    elif re.match("canada|can",lowercase):
-        abbrev = "ca" 
-    elif re.match(r"^at\b",lowercase):
-        abbrev = "atlantic" 
-    elif "pr" in lowercase:
-        abbrev = "prairies" 
-    elif "terr" in lowercase:
-        abbrev = "territories" 
-    else:
-        abbrev=lowercase
-    return(abbrev)
+    new_name=unidecode(lowercase) 
+    new_name=re.sub(' +', ' ', new_name)
+    
+    keys = (re.escape(k) for k in GEOS.keys())
+    pattern = re.compile(r'^\b(' + '|'.join(keys) + r')\b$')
+
+    result = pattern.sub(lambda x: GEOS[x.group()], new_name)
+    
+    if result == new_name:
+        result = lowercase
+    return(result)
 
 def create_geo_types(geo,default_geo):
-    regions = ['atlantic','atl','province of québec','québec','qc','province of ontario','ontario','on',
-               'prairies', 'pr', "british columbia", 'bc',"territories",'terr']
-    nation = ["canada","can",'ca']
-                    
-    if geo in nation:
+    if geo in NATION:
         geo_type="nation"
-    elif geo in regions:
+    elif geo in REGIONS:
         geo_type="region"
     else:
         geo_type = default_geo
@@ -101,18 +67,97 @@ def check_date_format(date_string):
 
     return(new_date)
 
-def get_revised_data(base_url):
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
-    }
-    
+def get_dashboard_update_date(base_url,headers):
     # Get update date
-    update_date_url =  base_url + "RVD_UpdateDate.csv"
+    update_date_url =  base_url + DASHBOARD_UPDATE_DATE_FILE
     update_date_url_response = requests.get(update_date_url, headers=headers)
     update_date = datetime.strptime(update_date_url_response.text,"%m/%d/%Y %H:%M:%S").strftime("%Y-%m-%d")
+    return(update_date)
+
+def check_most_recent_update_date(date,date_file):
+    with open(date_file) as file:
+        current_date = date
+        contents = file.read()
+        
+    already_updated = current_date in contents
+    return(already_updated)
+            
+def preprocess_table_columns(table):
+    """ 
+    Remove characters like . or * from columns
+    Abbreviate the viruses in columns
+    Change some naming of signals in columns (i.e order of hpiv and other)
+    Change some naming of locations in columns (i.e at instead of atl)
+    """
+    table.columns = [re.sub("\xa0"," ", col) for col in table.columns] # \xa0 to space
+    table.columns = [re.sub("(.*?)(\.\d+)", "\\1", c) for c in table.columns] # remove .# for duplicated columns
+    table.columns =[re.sub("\.", "", s)for s in table.columns] #remove periods
+    table.columns =[re.sub(r"\((all)\)", "", s)for s in table.columns] # remove (all) 
+    table.columns =[re.sub(r"\s*\(|\)", "", s)for s in table.columns]
+    table.columns = [re.sub(' +', ' ', col) for col in table.columns] # Make any muliple spaces into one space
+    table.columns = [re.sub(r'\(|\)', '', col) for col in table.columns] # replace () for _
+    table.columns = [re.sub(r'/', '_', col) for col in table.columns] # replace / with _
     
+    table.columns = [re.sub(r"^at\b","atl ",t) for t in table.columns]
+    table.columns = [re.sub("canada","can",t) for t in table.columns]
+    table.columns = [re.sub(r"\bcb\b","bc",t) for t in table.columns]
+    
+    table.columns =[re.sub(r"h1n1 2009 |h1n12009|a_h1|ah1\b", "ah1n1pdm09", s)for s in table.columns]
+    table.columns =[re.sub(r"a_uns", "auns", s)for s in table.columns]
+    table.columns =[re.sub(r"a_h3", "ah3", s)for s in table.columns]
+    
+    table.columns =[abbreviate_virus(col) for col in table.columns] # abbreviate viruses
+    table.columns = [re.sub(r"flu a","flua",t) for t in table.columns]
+    table.columns = [re.sub(r"flu b","flub",t) for t in table.columns]
+    table.columns = [re.sub(r"flutest\b","flu test", col) for col in table.columns]
+    table.columns = [re.sub(r"other hpiv|other_hpiv|hpiv other|hpiv_other","hpivother",t) for t in table.columns]
+    
+    table.columns=[re.sub(r'bpositive','b_positive',c) for c in table.columns]
+    table.columns=[re.sub(r'apositive','a_positive',c) for c in table.columns]
+    table.columns=[re.sub(r'hpiv_1','hpiv1',c) for c in table.columns]
+    table.columns=[re.sub(r'hpiv_2','hpiv2',c) for c in table.columns]
+    table.columns=[re.sub(r'hpiv_3','hpiv3',c) for c in table.columns]
+    table.columns=[re.sub(r'hpiv_4','hpiv4',c) for c in table.columns]
+    
+    table.columns=[make_signal_type_spelling_consistent(col) for col in table.columns]
+    return(table)
+
+def add_flu_prefix(flu_subtype):
+    """ Add the prefix `flu` when only the subtype is reported """
+    
+    pat1 =r"^ah3"
+    pat2= r"^auns" 
+    pat3= r"^ah1pdm09"
+    pat4= r"^ah1n1pdm09"
+    combined_pat = '|'.join((pat1, pat2,pat3,pat4))
+    
+    full_fluname = re.sub(combined_pat, r"flu\g<0>",flu_subtype)
+    return(full_fluname)
+
+def make_signal_type_spelling_consistent(signal):
+    """
+    Make the signal type (i.e. percent positive, number tests, total tests) have consistent spelling
+    Also remove total from signal names
+    """
+    
+    pat1 = r"positive\b"
+    pat2 = r'pos\b'
+    combined_pat = '|'.join((pat1, pat2))
+    
+    pat3 = r"test\b"
+    pat4 = 'tested'
+    combined_pat2 = '|'.join((pat3, pat4))
+    
+    new_signal = re.sub(combined_pat, "positive_tests",signal)
+    new_signal = re.sub(combined_pat2, "tests",new_signal)
+    new_signal =re.sub(" *%", "_pct_positive",new_signal)
+    new_signal = re.sub("total ", "",new_signal)
+    return(new_signal)
+    
+
+def get_positive_data(base_url,headers,update_date):
     # Get update data
-    url = base_url+"RVD_WeeklyData.csv"
+    url = base_url+DASHBOARD_DATA_FILE
 
     url_response = requests.get(url, headers=headers)
     df = pd.read_csv(io.StringIO(url_response.text))
@@ -127,12 +172,16 @@ def get_revised_data(base_url):
     df['geo_type'] = [create_geo_types(g,"province") for g in df['geo_value']]
     df.insert(1,"issue",update_date)
     
-    df=df.drop(["weekorder","region","year","week"],axis=1)
+    #df=df.drop(["weekorder","region","year","week"],axis=1)
     
-    df = df.pivot(index=['epiweek','time_value','issue','geo_type','geo_value'],
+    df = df.pivot(index=['epiweek','time_value','issue','geo_type','geo_value','region','week','weekorder','year'],
                   columns="virus",values=['tests','percentpositive','positivetests'])
+    
     df.columns = ['_'.join(col).strip() for col in df.columns.values]
     df = df.rename(columns=lambda x: '_'.join(x.split('_')[1:]+x.split('_')[:1]))
+    df.columns = [re.sub(r'/', '', col) for col in df.columns] # replace / with _
+    df.columns = [re.sub(r"flu a","flua",t) for t in df.columns]
+    df.columns = [re.sub(r"flu b","flub",t) for t in df.columns]
     df.columns=[re.sub("positivetests", "positive_tests",col) for col in df.columns]
     df.columns=[re.sub("percentpositive", "pct_positive",col) for col in df.columns]
     df.columns=[re.sub(r' ','_',c) for c in df.columns]
@@ -142,17 +191,8 @@ def get_revised_data(base_url):
             assert all([0 <= val <= 100 or math.isnan(val) for val in  df[df.columns[k]]]), "Percentage not from 0-100"
     
     return(df)
-    
-def get_weekly_data(base_url,start_year):
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
-    }
-    
-    # Get update date
-    update_date_url =  base_url + "RVD_UpdateDate.csv"
-    update_date_url_response = requests.get(update_date_url, headers=headers)
-    update_date = datetime.strptime(update_date_url_response.text,"%m/%d/%Y %H:%M:%S").strftime("%Y-%m-%d")
 
+def get_detections_data(base_url,headers,update_date):
     # Get current week and year
     summary_url =  base_url + "RVD_SummaryText.csv"
     summary_url_response = requests.get(summary_url, headers=headers)
@@ -161,55 +201,48 @@ def get_weekly_data(base_url,start_year):
     week_df = summary_df[(summary_df['Section'] == "summary") & (summary_df['Type']=="title")]
     week_string = week_df.iloc[0]['Text'].lower()
     current_week = int(re.search("week (.+?) ", week_string).group(1))
-
-    if current_week < 35:
-        current_year = start_year+1
-    else:
-        current_year = start_year
+    current_year= int(re.search("20\d{2}", week_string).group(0))
         
     current_epiweek= Week(current_year,current_week)
    
     # Get weekly data
-    weekly_url = base_url + "RVD_CurrentWeekTable.csv"
-    weekly_url_response = requests.get(weekly_url, headers=headers)
-    weekly_url_response.encoding='UTF-8'
-    df_weekly = pd.read_csv(io.StringIO(weekly_url_response.text))
+    detections_url = base_url + "RVD_CurrentWeekTable.csv"
+    detections_url_response = requests.get(detections_url, headers=headers)
+    detections_url_response.encoding='UTF-8'
+    df_detections = pd.read_csv(io.StringIO(detections_url_response.text))
     
-    df_weekly = df_weekly.rename(columns=lambda x: '_'.join(x.split('_')[1:]+x.split('_')[:1]))
-    df_weekly.insert(0,"epiweek",int(str(current_epiweek)))
-    df_weekly.insert(1,"time_value",str(current_epiweek.enddate()))
-    df_weekly.insert(2,"issue",update_date)
-    df_weekly.columns=[abbreviate_virus(c) for c in df_weekly.columns]
-    df_weekly.columns=[re.sub(r'test\b','tests',c) for c in df_weekly.columns]
-    df_weekly.columns=[re.sub(r'pos\b','positive_tests',c) for c in df_weekly.columns]
-    df_weekly.columns=[re.sub(r'flua_','flu_a',c) for c in df_weekly.columns]
-    df_weekly.columns=[re.sub(r'flub_','flu_b',c) for c in df_weekly.columns]
-    df_weekly.columns=[re.sub(r'bpositive','b_positive',c) for c in df_weekly.columns]
-    df_weekly.columns=[re.sub(r'apositive','a_positive',c) for c in df_weekly.columns]
-    df_weekly.columns=[re.sub(r'flu_ah1_','flu_ah1pdm09_',c) for c in df_weekly.columns]
-    df_weekly.columns=[re.sub(r' ','_',c) for c in df_weekly.columns]
-    df_weekly=df_weekly.rename(columns={'reportinglaboratory':"geo_value"})
-    df_weekly['geo_value'] = [abbreviate_geo(g) for g in df_weekly['geo_value']]
-    df_weekly['geo_type'] = [create_geo_types(g,"lab") for g in df_weekly['geo_value']]
+    df_detections = df_detections.rename(columns=lambda x: '_'.join(x.split('_')[1:]+x.split('_')[:1]))
+    df_detections.insert(0,"epiweek",int(str(current_epiweek)))
+    df_detections.insert(1,"time_value",str(current_epiweek.enddate()))
+    df_detections.insert(2,"issue",update_date)
+    df_detections=preprocess_table_columns(df_detections)
     
-    df_weekly=df_weekly.drop(["weekorder","date","week"],axis=1)
+    df_detections.columns=[re.sub(r' ','_',c) for c in df_detections.columns]
+    df_detections=df_detections.rename(columns={'reportinglaboratory':"geo_value"})
+    df_detections['geo_value'] = [abbreviate_geo(g) for g in df_detections['geo_value']]
+    df_detections['geo_type'] = [create_geo_types(g,"lab") for g in df_detections['geo_value']]
 
-    return(df_weekly)
+    return(df_detections.set_index(['epiweek', 'time_value', 'issue', 'geo_type', 'geo_value']))
 
-base_url = "https://health-infobase.canada.ca/src/data/respiratory-virus-detections/"
+# Get data
 
-weekly_data = get_weekly_data(base_url,2024).set_index(['epiweek', 'time_value', 'issue', 'geo_type', 'geo_value'])
-positive_data = get_revised_data(base_url)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+    }
+
+update_date = get_dashboard_update_date(DASHBOARD_BASE_URL, headers)
+detections_data = get_detections_data(DASHBOARD_BASE_URL,headers,update_date)
+positive_data = get_positive_data(DASHBOARD_BASE_URL,headers,update_date)
 
 path1 = './data/season_2024_2025/respiratory_detections.csv'
 path2 = './data/season_2024_2025/positive_tests.csv'
 
 if os.path.exists(path1)==False:
-    weekly_data.to_csv(path1,index=True)
+    detections_data.to_csv(path1,index=True)
 else:
     old_detection_data = pd.read_csv(path1).set_index(['epiweek', 'time_value', 'issue', 'geo_type', 'geo_value'])
-    if weekly_data.index.isin(old_detection_data.index).any() == False:
-        old_detection_data= pd.concat([old_detection_data,weekly_data],axis=0)
+    if detections_data.index.isin(old_detection_data.index).any() == False:
+        old_detection_data= pd.concat([old_detection_data,detections_data],axis=0)
         old_detection_data.to_csv(path1,index=True)
 
 if os.path.exists(path2)==False:
